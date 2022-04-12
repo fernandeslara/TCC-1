3918dae9626ebf06d8b116c9cc9c472d5233abe3
GuiCintra27 <86432794+GuiCintra27@users.noreply.github.com>
2022-04-12 07:18:03 -0300

    Add files via upload

---
+++ Menu.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    let menu = document.getElementById("Menu");
+    let bar = document.getElementById("Menu-bar");
+    bar.classList.toggle('active');
+    menu.classList.toggle('active');
+}
